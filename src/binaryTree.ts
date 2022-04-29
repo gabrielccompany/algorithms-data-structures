ead6ac1c525742075c46c156306aca206a51ef36
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2022-04-29 09:06:11 -0300

    test 6

---
+++ src/binaryTree.ts
@@ -0,0 +1,48 @@
+interface Node {
+  left?: Node;
+  right?: Node;
+  value: number;
+}
+
+export default class BTHeigth {
+  input: number[];
+  node: Node;
+
+  constructor(input: number[]) {
+    this.input = input;
+  }
+
+  recursiveBuildTree(queue: number[], node: Node): Node {
+    const current = queue.shift();
+
+    if (!current) return node;
+
+    if (current < node.value && !node.left) {
+      node.left = { value: current };
+      return this.recursiveBuildTree(queue, node.left);
+    }
+
+    if (current > node.value && !node.right) {
+      node.right = { value: current };
+      return this.recursiveBuildTree(queue, node.right);
+    }
+
+    return node;
+  }
+
+  main() {
+    // Doing this because side effect of the orinal array (shift removes the first element)
+    const treeNodesArray = Array.from(this.input);
+    const rootValue = treeNodesArray.shift();
+    const initialNode: Node = { value: rootValue };
+    if (!rootValue) return 0;
+
+    this.recursiveBuildTree(treeNodesArray, initialNode);
+
+    return this.input;
+  }
+}
+
+// test2
+
+
